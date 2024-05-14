8a986cf5c24e91011de14a45496116550392a342
장정안 <wjddks96@naver.com>
2024-05-15 04:37:23 +0900

    feat: 헤더 합성 컴포넌트로 구현

---
+++ src/components/@common/button/AppOpenBtn.tsx
@@ -0,0 +1,26 @@
+import { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+
+const AppOpenBtn = (props: PropsWithChildren) => {
+  const { children, ...restProps } = props;
+  return <AppOpenBtnContainer {...restProps}>{children}</AppOpenBtnContainer>;
+};
+
+const AppOpenBtnContainer = styled.article`
+  ${({ theme }) =>
+    theme.mixin.inlineFlexBox({ align: 'center', justify: 'center' })};
+  padding: 0.5rem 1.6rem;
+  border-radius: 70px;
+  gap: 1rem;
+  background: ${({ theme }) => theme.colors.white};
+  cursor: pointer;
+`;
+
+const AppOpenBtnText = styled.p`
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+AppOpenBtn.Wrapper = AppOpenBtn;
+AppOpenBtn.Text = AppOpenBtnText;
+
+export { AppOpenBtn };
---
+++ src/components/@common/header/Header.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { AppOpenBtn } from '../button/AppOpenBtn';
+import { HeaderRoot } from './HeaderWrapper';
+
+const Header = () => {
+  return (
+    <>
+      <HeaderRoot>
+        <HeaderRoot.Logo />
+        <HeaderMenuContainer>
+          <IconButtonContainer>
+            <HeaderRoot.Icon />
+            <AppOpenBtn>
+              <AppOpenBtn.Wrapper>
+                <AppOpenBtn.Text>앱 열기</AppOpenBtn.Text>
+              </AppOpenBtn.Wrapper>
+            </AppOpenBtn>
+          </IconButtonContainer>
+          <HeaderRoot.Menu />
+        </HeaderMenuContainer>
+      </HeaderRoot>
+    </>
+  );
+};
+
+export default Header;
+
+const HeaderMenuContainer = styled.article`
+  ${({ theme }) => theme.mixin.flexCenter({ direction: 'row' })};
+`;
+
+const IconButtonContainer = styled.div`
+  ${({ theme }) => theme.mixin.flexCenter({ direction: 'row' })};
+  gap: 1.5rem;
+`;
