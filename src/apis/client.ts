add82d58dba5b02cc5867137ea2c0669f3d76521
장정안 <wjddks96@naver.com>
2024-05-14 17:06:29 +0900

    feat: axios instance 생성 및 intercepter를 이용하여 자동으로 헤더에 memberId 추가해서 서버와 통신

---
+++ src/apis/client.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export const instance = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 요청을 보내기 전에 요청 인터셉터를 사용하여 memberId를 헤더에 추가
+instance.interceptors.request.use(
+  (config) => {
+    const memberId = 1; // 여기에 멤버 아이디를1로 설정한거임
+    config.headers['memberId'] = memberId;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export function get<T>(...args: Parameters<typeof instance.get>) {
+  return instance.get<T>(...args);
+}
+
+export function post<T>(...args: Parameters<typeof instance.post>) {
+  return instance.post<T>(...args);
+}
+
+export function del<T>(...args: Parameters<typeof instance.delete>) {
+  return instance.delete<T>(...args);
+}
