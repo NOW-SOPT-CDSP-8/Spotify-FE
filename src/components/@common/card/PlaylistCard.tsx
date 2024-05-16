d811254cefd690d10c3ac1f034cf1e3b38b6630b
또이 <90364711+imddoy@users.noreply.github.com>
2024-05-16 19:35:15 +0900

    [style] 공통컴포넌트 - 플레이리스트 카드 퍼블리싱 (#25)
    * style: 플레이리스트 카드 퍼블리싱
    
    * feat: cardList 재사용 가능하게 수정
    호출 측에서 props로 json을 넘겨주면 재사용이 가능함
    
    * docs: 폴더 위치 변경
    playlist에서 category로 이동

---
+++ src/components/@common/card/PlaylistCard.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface PlaylistCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+  hasDescription: boolean;
+}
+
+const PlaylistCard = ({ children, hasDescription }: PlaylistCardProps) => {
+  const borderRadius = hasDescription ? '0.4rem' : '0.5rem';
+  const marginBottom = hasDescription ? '0.7rem' : '1rem';
+
+  return (
+    <PlaylistCardWrapper>
+      {React.Children.map(children, (child) => {
+        // PlaylistCardImg인지 확인
+        if (React.isValidElement(child) && child.type === PlaylistCardImg) {
+          // style 속성 주며 클론
+          return React.cloneElement(child as JSX.Element, {
+            borderRadius,
+            marginBottom,
+          });
+        }
+        return child;
+      })}
+    </PlaylistCardWrapper>
+  );
+};
+
+export default PlaylistCard;
+
+const PlaylistCardWrapper = styled.section`
+  width: 14.6rem;
+`;
+
+interface PlaylistCardImgProps {
+  borderRadius?: string;
+  marginBottom?: string;
+}
+const PlaylistCardImg = styled.img<PlaylistCardImgProps>`
+  width: 14.6rem;
+  height: 14.6rem;
+  margin-bottom: ${({ marginBottom }) => marginBottom};
+  border-radius: ${({ borderRadius }) => borderRadius};
+`;
+
+const PlaylistCardTitle = styled.h1`
+  max-width: 90%;
+  margin-bottom: 0.3rem;
+
+  ${({ theme }) => theme.fonts.detail1};
+  color: ${({ theme }) => theme.colors.white};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
+
+const PlaylistCardDescription = styled.p`
+  ${({ theme }) => theme.fonts.detail2};
+  color: ${({ theme }) => theme.colors.gray200};
+`;
+
+PlaylistCard.Img = PlaylistCardImg;
+PlaylistCard.Title = PlaylistCardTitle;
+PlaylistCard.Description = PlaylistCardDescription;
---
+++ src/components/@common/card/PlaylistCardList.tsx
@@ -0,0 +1,45 @@
+import PlaylistCard from './PlaylistCard';
+import styled from 'styled-components';
+
+type listData = {
+  imgUrl: string;
+  title: string;
+  description?: string;
+};
+
+interface PlaylistCardListProps {
+  datas: listData[];
+}
+
+const PlaylistCardList = ({ datas }: PlaylistCardListProps) => {
+  return (
+    <PlaylistCardWrapper>
+      {datas.map((data) => (
+        <PlaylistCard hasDescription={data.description ? true : false}>
+          <PlaylistCard.Img src={data.imgUrl} />
+          <PlaylistCard.Title>{data.title}</PlaylistCard.Title>
+          {data.description && (
+            <PlaylistCard.Description>
+              {data.description}
+            </PlaylistCard.Description>
+          )}
+        </PlaylistCard>
+      ))}
+    </PlaylistCardWrapper>
+  );
+};
+
+export default PlaylistCardList;
+
+const PlaylistCardWrapper = styled.section`
+  ${({ theme }) => theme.mixin.flexBox({ align: 'start', justify: 'start' })};
+  gap: 1.5rem;
+  margin-top: 2rem;
+  padding: 0 1.5rem;
+  overflow-x: scroll;
+  overflow-y: hidden;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
