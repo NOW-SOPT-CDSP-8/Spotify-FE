e58f16f8640748554e3b89e37b627d854f2ea0f1
장정안 <wjddks96@naver.com>
2024-05-16 21:54:00 +0900

    style: 홈페이지 내 추천 스테이션 컴포넌트 ui 구현

---
+++ src/components/homePage/RecommendStation.tsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import PlaylistCardList from '../@common/card/PlaylistCardList';
+import { predata } from '../../mocks/mockData';
+
+interface RecommendStationProps {}
+
+const RecommendStation = ({}: RecommendStationProps) => {
+  return (
+    <RecommendStationWrapper>
+      <TitleContainer>
+        <HeadLine>추천 스테이션</HeadLine>
+        {/* 정안TODO 멤버ID가 1인 사람의 이름을 GET해와서 '선정님' 대신에 집어넣어줘야하나? */}
+        <Caption>선정님 좋아하는 곡과 비슷한 곡을 큐레이팅 했어요</Caption>
+      </TitleContainer>
+      {/* 정안TODO predata대신 서버에서 GET해오기 */}
+      <PlaylistCardListContainer>
+        <PlaylistCardList datas={predata} />
+      </PlaylistCardListContainer>
+    </RecommendStationWrapper>
+  );
+};
+
+export default RecommendStation;
+
+const RecommendStationWrapper = styled.article`
+  ${({ theme }) =>
+    theme.mixin.inlineFlexBox({
+      direction: 'column',
+      align: 'flex-start',
+      justify: 'flex-start',
+    })};
+  width: 100%;
+  gap: 2rem;
+`;
+
+const HeadLine = styled.h1`
+  ${({ theme }) => theme.fonts.headline1};
+  color: ${({ theme }) => theme.colors.white};
+  margin-top: 2.9rem;
+`;
+
+const Caption = styled.p`
+  ${({ theme }) => theme.fonts.caption1};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+const TitleContainer = styled.div`
+  ${({ theme }) =>
+    theme.mixin.flexBox({ direction: 'column', align: 'flex-start' })};
+  gap: 0.4rem;
+`;
+
+const PlaylistCardListContainer = styled.div`
+  ${({ theme }) => theme.mixin.flexBox({ align: 'start', justify: 'start' })};
+  width: 90%;
+  overflow-x: scroll;
+  overflow-y: hidden;
+`;
--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,7 +1,20 @@
+import styled from 'styled-components';
+import RecommendStation from '../../components/homePage/RecommendStation';
+import ExploreSection from './ExploreSection';
+
 interface HomePageProps {}
 
 const HomePage = ({}: HomePageProps) => {
-  return <></>;
+  return (
+    <HomePageWrapper>
+      <RecommendStation />
+      <ExploreSection />
+    </HomePageWrapper>
+  );
 };
 
 export default HomePage;
+
+const HomePageWrapper = styled.section`
+  margin-left: 1.6rem;
+`;
