3c5e1abafdaeaaac82d697a304af9d7076456a28
장정안 <wjddks96@naver.com>
2024-05-16 16:24:42 +0900

    style: 메뉴 navbar ui 구현

--- src/components/@common/menu/Menu.tsx
+++ src/components/@common/menu/Menu.tsx
@@ -1,16 +1,37 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import { IcClose } from '../../../assets/svg';
+import { IcClose, IcGo, IcUnderbar } from '../../../assets/svg';
+import { useMobile } from '../../../utils/useMobile';
 
 interface MenuProps {}
 
 const Menu = ({}: MenuProps) => {
+  useMobile();
   const [visible, setVisible] = useState(false);
 
   return (
-    <MenuWrapper>
-      <CloseBtn />
-    </MenuWrapper>
+    <>
+      <MenuWrapper>
+        <CloseBtn />
+        <TitleContainer>
+          <MainTitleGoIconContainer>
+            <MainTitle>계정 보기</MainTitle>
+            <AccountGoIcon />
+          </MainTitleGoIconContainer>
+          <MainTitle>프로필</MainTitle>
+          <MainTitle>로그아웃 하기</MainTitle>
+        </TitleContainer>
+        <UnderbarIconContainer>
+          <UnderbarIcon />
+        </UnderbarIconContainer>
+        <SubTitleContainer>
+          <SubTitle>지원</SubTitle>
+          <SubTitle>다운로드하기</SubTitle>
+          <SubTitle>개인정보취급방침</SubTitle>
+          <SubTitle>약관</SubTitle>
+        </SubTitleContainer>
+      </MenuWrapper>
+    </>
   );
 };
 
@@ -19,7 +40,7 @@ export default Menu;
 const MenuWrapper = styled.section`
   background-color: ${({ theme }) => theme.colors.black};
   width: 100%;
-  height: auto;
+  height: 100vh;
 `;
 
 const CloseBtn = styled(IcClose)`
@@ -27,4 +48,56 @@ const CloseBtn = styled(IcClose)`
   height: 2.4rem;
   margin-left: 33.4rem;
   margin-top: 1.97rem;
+`;
+
+const TitleContainer = styled.article`
+  ${({ theme }) =>
+    theme.mixin.inlineFlexBox({
+      direction: 'column',
+      justify: 'center',
+    })};
+  margin-left: 3.6rem;
+  margin-top: 1.9rem;
+  gap: 2rem;
+`;
+
+const MainTitle = styled.p`
+  color: ${({ theme }) => theme.colors.white};
+  ${({ theme }) => theme.fonts.title4};
+`;
+
+const AccountGoIcon = styled(IcGo)`
+  width: 2rem;
+  height: 2rem;
+`;
+
+const MainTitleGoIconContainer = styled.div`
+  ${({ theme }) => theme.mixin.flexBox({ direction: 'row', align: 'center' })};
+  gap: 0.7rem;
+`;
+
+const UnderbarIcon = styled(IcUnderbar)`
+  width: 1.4rem;
+`;
+
+const UnderbarIconContainer = styled.article`
+  ${({ theme }) => theme.mixin.flexBox({ direction: 'row', align: 'center' })};
+  margin-top: 3.5rem;
+  margin-left: 3.6rem;
+`;
+
+const SubTitle = styled.p`
+  ${({ theme }) => theme.fonts.headline2};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+const SubTitleContainer = styled.article`
+  ${({ theme }) =>
+    theme.mixin.inlineFlexBox({
+      direction: 'column',
+      justify: 'center',
+    })};
+  margin-left: 3.6rem;
+  margin-top: 3rem;
+  gap: 2rem;
 `;
