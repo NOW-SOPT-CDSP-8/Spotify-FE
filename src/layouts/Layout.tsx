46bea967afcf663650a5927f08a469c036f2a910
장정안 <126966681+ExceptAnyone@users.noreply.github.com>
2024-05-16 15:35:12 +0900

    style: 전체를 감싸는 wrapper 높이 값 고정 및 배경색 지정 (#27)

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,15 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import { useMobile } from '../utils/useMobile';
 import Header from '../components/@common/header/Header';
+import styled from 'styled-components';
 
 const Layout = () => {
   useMobile();
   return (
-    <>
+    <Wrapper>
       <Header />
       <Outlet />
-    </>
+    </Wrapper>
   );
 };
 
 export default Layout;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.gray900};
+`;
