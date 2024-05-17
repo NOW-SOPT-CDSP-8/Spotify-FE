daae5137b44e064266be088d2dc6f3de7b3ec5de
Kjiw0n <rwd0904@naver.com>
2024-05-17 21:23:32 +0900

    style: PlayList 화면 구성

--- src/pages/playlistPage/PlaylistPage.tsx
+++ src/pages/playlistPage/PlaylistPage.tsx
@@ -1,7 +1,49 @@
+import styled from 'styled-components';
+import InfoComponent from '../../components/playlistPage/InfoComponent';
+import LogoComponent from '../../components/playlistPage/LogoComponent';
+import MenuComponent from '../../components/playlistPage/MenuComponent';
+import MusicListComponent from '../../components/playlistPage/MusicListComponent';
+import PlayComponent from '../../components/playlistPage/PlayComponent';
+
 interface PlaylistPageProps {}
 
 const PlaylistPage = ({}: PlaylistPageProps) => {
-  return;
+  return (
+    <PlayListPageWrapper>
+      <CardImgContainer>
+        <img src='src/assets/png/PlayListCard.png' alt='Album cover' />
+      </CardImgContainer>
+      <InfoComponent />
+      <LogoComponent />
+      <MenuComponent />
+      <MusicListComponent />
+      <PlayComponent />
+    </PlayListPageWrapper>
+  );
 };
 
 export default PlaylistPage;
+
+const PlayListPageWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  flex-shrink: 0;
+  gap: 0.4rem;
+  overflow-y: auto;
+  ${({ theme }) =>
+    theme.mixin.flexBox({
+      direction: 'column',
+      align: 'flex-start',
+    })};
+  background: linear-gradient(180deg, #645e33 0%, #121212 54.3%);
+`;
+
+const CardImgContainer = styled.div`
+  width: 100%;
+  margin-top: 1.1rem;
+  ${({ theme }) => theme.mixin.flexCenter({ direction: 'column' })};
+  img {
+    width: 15rem;
+    height: 15rem;
+  }
+`;
