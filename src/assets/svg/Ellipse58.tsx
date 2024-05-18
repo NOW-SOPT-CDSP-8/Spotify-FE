a7d1c7d012c1a058dc3dabd7cd0336f64282c78f
Kjiw0n <rwd0904@naver.com>
2024-05-18 15:23:37 +0900

    chore: Mypage 구성 아이콘 및 카드이미지 추가

---
+++ src/assets/svg/Ellipse58.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgEllipse58 = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 33 33'
+    {...props}
+  >
+    <circle
+      cx={16.636}
+      cy={16.87}
+      r={14.068}
+      fill='#4E9AF5'
+      stroke='#000'
+      strokeWidth={4}
+    />
+  </svg>
+);
+export default SvgEllipse58;
