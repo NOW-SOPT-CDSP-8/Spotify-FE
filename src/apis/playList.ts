42c9400700c4531b90f376cb769c874bc7eb58f2
Kjiw0n <rwd0904@naver.com>
2024-05-23 06:19:48 +0900

    feat: 플레이리스트 api 연결

---
+++ src/apis/playList.ts
@@ -0,0 +1,20 @@
+import { get } from './client';
+
+export const fetchMusics = async ({ pageParam = -1 }) => {
+  const response = await get('api/v1/1/musics', {
+    params: {
+      cursor: pageParam,
+    },
+  });
+
+  if (!response.data.data || response.data.data.length === 0) {
+    console.log('음악이 없음');
+  }
+
+  return {
+    items: response.data.data,
+    nextPage: response.data.data.length
+      ? response.data.data.slice(-1)[0].musicId
+      : undefined,
+  };
+};
