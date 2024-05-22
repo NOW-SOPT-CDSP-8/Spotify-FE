f4858044db42b104ae94565ce84d1b92d25b48c6
장정안 <wjddks96@naver.com>
2024-05-23 01:01:17 +0900

    feat: 필터링 api 생성

--- src/apis/music.ts
+++ src/apis/music.ts
@@ -1,8 +1,23 @@
-import { GetMusicTitleReq } from '../types/music/client';
+import {
+  GetFilterTitleReq,
+  GetMusicTitleReq,
+  genre,
+} from '../types/music/client';
 import { get } from './client';
 
 export const getMusicTitle = async () => {
   const { data } = await get<GetMusicTitleReq>('api/v1/stations');
+
+  return data;
+};
+
+export const getMusicTitleWithPlaylist = async (
+  filterId: number,
+  genre: genre,
+) => {
+  const { data } = await get<GetFilterTitleReq>(
+    `api/v1/musics/filter/${filterId}?genre=${genre}`,
+  );
 
   return data;
 };
