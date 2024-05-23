bba2ec7f0f4fc69b0c6f4434b4ffdd0a4fa67f13
Kjiw0n <rwd0904@naver.com>
2024-05-23 15:51:35 +0900

    refactor: queryKey 변수로 저장

--- src/hooks/queries/useMusicInfiniteQuery.tsx
+++ src/hooks/queries/useMusicInfiniteQuery.tsx
@@ -1,9 +1,11 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { fetchMusics } from '../../apis/playList';
 
+const QUERY_KEY_MUSICS = ['musics'];
+
 const useMusicInfiniteQuery = (cursor) => {
   return useInfiniteQuery({
-    queryKey: ['musics'],
+    queryKey: QUERY_KEY_MUSICS,
     queryFn: ({ pageParam = cursor }) => fetchMusics({ pageParam }),
     getNextPageParam: (lastPage, allPages) => lastPage.nextPage,
     initialPageParam: 0,
