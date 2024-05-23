53702a0b96807a04c219e8447d7bbfab1d89a9f0
Kjiw0n <128016888+Kjiw0n@users.noreply.github.com>
2024-05-23 16:03:34 +0900

    [feat] 마이페이지 API 연결 (#49)
    * 마이페이지 API 연결
    
    * style: 카드 이미지 svg로 추가
    
    * feat: api 연결 반영
    
    * refactor: clients.ts 파일의 axios instance 이용하여 api 연결
    
    * feat: 리엑트쿼리 이용해 프로필 데이터 가져오는 훅 생성
    
    * refactor: 리엑트 쿼리를 적용하여 api 연결방식 개선
    
    * fix: develop 병합 충돌 해결
    
    * chore: 파일 이름 및 위치 변경
    
    * refactor: 리엑트 쿼리로 인해 필요하지않은 useState, useEffect 제거

---
+++ src/assets/svg/IcCardSampleImg.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgIcCardSampleImg = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 38 59'
+    {...props}
+  >
+    <rect width={37} height={58} x={0.568} y={0.283} fill='#000' rx={6} />
+  </svg>
+);
+export default SvgIcCardSampleImg;
