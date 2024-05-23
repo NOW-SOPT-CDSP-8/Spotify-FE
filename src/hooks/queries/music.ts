b101123ae6fd43e977a38c66be55e27df89983b7
장정안 <126966681+ExceptAnyone@users.noreply.github.com>
2024-05-23 16:07:22 +0900

    [Feat] PlaylistCardList 공통 컴포넌트 재설계 실패로 mockData 작업 진행 (#55)
    * feat: 서버 데이터에 맞게 타입 추가
    
    * feat: mockData 이미지 url 변경
    
    * feat: 필터링 api 생성
    
    * feat: PlaylistCardListWithDescription 컴포넌트 복붙
    
    * feat: react query를 이용한 필터 함수 작성
    
    * feat: CategoryPage 내 GET으로 요청
    
    * feat: genre 타입 설정
    
    * feat: 예시로 필터id는 1, 장르는 pop으로 GET

--- src/hooks/queries/music.ts
+++ src/hooks/queries/music.ts
@@ -1,15 +1,28 @@
 import { useQuery } from '@tanstack/react-query';
-import { getMusicTitle } from '../../apis/music';
+import { getMusicTitle, getMusicTitleWithPlaylist } from '../../apis/music';
+import { genre } from '../../types/music/client';
 
 const QUERY_KEY = {
   GET_MUSIC_TITLE: ['musicTitle'],
+  GET_PLAYLIST: ['playlist'],
 };
 
 export const useGetMusicTitle = () => {
   const { data, isLoading } = useQuery({
     queryKey: QUERY_KEY.GET_MUSIC_TITLE,
-    //쿼리키가 바뀌면 무조건 재요청, 동적으로 쿼리키를 해야할 때가 있음. 함수인자로 받자.
     queryFn: getMusicTitle,
   });
+  return { data, isLoading };
+};
+
+export const useGetMusicTitleWithPlaylist = (
+  filterId: number,
+  genre: genre,
+) => {
+  const { data, isLoading } = useQuery({
+    queryKey: QUERY_KEY.GET_PLAYLIST,
+    queryFn: () => getMusicTitleWithPlaylist(filterId, genre),
+  });
+
   return { data, isLoading };
 };
--- src/mocks/mockData.ts
+++ src/mocks/mockData.ts
@@ -1,58 +1,73 @@
 export const predata = [
   {
+    id: 1,
     imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
     title: 'Sunny Beach',
     description: 'A beautiful sunny beach with crystal clear water.',
   },
   {
-    imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
+    id: 2,
+    imgUrl: 'https://plestify.ir/wp-content/uploads/2022/06/K-Pop-Rising.jpg',
     title: 'Mountain Landscape',
     description: 'A breathtaking view of the mountain range during sunset.',
   },
   {
-    imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
+    id: 3,
+    imgUrl: 'https://pbs.twimg.com/media/F8JkJNma0AAyIbF.jpg:large',
     title: 'City Skyline',
     description: 'A panoramic view of a bustling city skyline at night.',
   },
-  {
-    imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
-    title: 'Forest Pathway',
-    description: 'A serene pathway through a dense forest with tall trees.',
-  },
-  {
-    imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
-    title: 'Forest Pathway',
-    description: 'A serene pathway through a dense forest with tall trees.',
-  },
-  {
-    imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
-    title: 'Forest Pathway',
-    description: 'A serene pathway through a dense forest with tall trees.',
-  },
+  // {
+  //   id: 4,
+  //   imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
+  //   title: 'Forest Pathway',
+  //   description: 'A serene pathway through a dense forest with tall trees.',
+  // },
+  // {
+  //   id: 5,
+  //   imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
+  //   title: 'Forest Pathway',
+  //   description: 'A serene pathway through a dense forest with tall trees.',
+  // },
+  // {
+  //   id: 6,
+  //   imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
+  //   title: 'Forest Pathway',
+  //   description: 'A serene pathway through a dense forest with tall trees.',
+  // },
 ];
 
 export const predata2 = [
   {
+    id: 1,
     imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
     title: 'Sunny Beach',
+    description: 'A beautiful sunny beach with crystal clear water.',
   },
   {
-    imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
+    id: 2,
+    imgUrl: 'https://plestify.ir/wp-content/uploads/2022/06/K-Pop-Rising.jpg',
     title: 'Mountain Landscape',
+    description: 'A breathtaking view of the mountain range during sunset.',
   },
   {
-    imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
+    id: 3,
+    imgUrl: 'https://pbs.twimg.com/media/F8JkJNma0AAyIbF.jpg:large',
     title: 'City Skyline',
+    description: 'A panoramic view of a bustling city skyline at night.',
   },
   {
+    id: 4,
     imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
     title: 'Forest Pathway',
   },
   {
+    id: 5,
     imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
     title: 'Forest Pathway',
   },
   {
+    id: 6,
     imgUrl: 'https://pbs.twimg.com/media/FYznstoXEAATx8k.jpg',
     title: 'Forest Pathway',
   },
