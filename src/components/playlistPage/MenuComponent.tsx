917a71cf3032c3a3e26a5688057d49ea7acfdf04
또이 <90364711+imddoy@users.noreply.github.com>
2024-05-23 16:08:33 +0900

    [feat] 좋아요 post delete 구현 (#54)
    * feat: client.ts 사용해서 좋아요 post, delete 구현
    
    * feat: 낙관적 업데이트 사용해서 post, delete 구현
    
    * feat: 리액트쿼리 - 낙관적 업데이트 좋아요 구현
    
    * etc: package.json 버전 수정
    
    * etc: 주석 제거
    
    * chore: 쿼리키 변수로 선언

--- src/components/playlistPage/MenuComponent.tsx
+++ src/components/playlistPage/MenuComponent.tsx
@@ -1,11 +1,74 @@
 import styled from 'styled-components';
 import { IcShare, IcMenuVertical } from '../../assets/svg';
 import LikeComponent from './LikeComponent';
+import { post, del } from '../../apis/client';
+import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const MenuComponent = () => {
+  const [liked, setLiked] = useState(true);
+  const [likeCount, setLikeCount] = useState(8);
+
+  const queryClient = useQueryClient();
+  const QUERY_KEY = {
+    LIKE_STATUS: 'likeStatus',
+  };
+
+  const useLikeMutation = useMutation({
+    mutationFn: () => post('/api/v1/1/liked'),
+    onMutate: async () => {
+      // 낙관적 업데이트: 서버 요청 전에 UI를 먼저 업데이트
+      setLiked(true);
+      setLikeCount((prevCount) => prevCount + 1);
+    },
+    onError: (error) => {
+      // 에러 발생 시 이전 상태로 되돌리기
+      setLiked(false);
+      setLikeCount((prevCount) => prevCount - 1);
+      console.error(error);
+    },
+    onSuccess: () => {
+      console.log('좋아요');
+    },
+    onSettled: () => {
+      // 성공 여부에 관계없이 쿼리 무효화
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.LIKE_STATUS] });
+    },
+  });
+
+  const useUnlikeMutation = useMutation({
+    mutationFn: () => del('/api/v1/1/unliked'),
+    onMutate: async () => {
+      // 낙관적 업데이트: 서버 요청 전에 UI를 먼저 업데이트
+      setLiked(false);
+      setLikeCount((prevCount) => prevCount - 1);
+    },
+    onError: (error) => {
+      // 에러 발생 시 이전 상태로 되돌리기
+      setLiked(true);
+      setLikeCount((prevCount) => prevCount + 1);
+      console.error(error);
+    },
+    onSuccess: () => {
+      console.log('좋아요 취소');
+    },
+    onSettled: () => {
+      // 성공 여부에 관계없이 쿼리 무효화
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.LIKE_STATUS] });
+    },
+  });
+
+  const toggleLike = () => {
+    if (liked) {
+      useUnlikeMutation.mutate();
+    } else {
+      useLikeMutation.mutate();
+    }
+  };
+
   return (
     <MenuComponentWrapper>
-      <LikeComponent likeCount={10} />
+      <LikeComponent likeCount={likeCount} onClick={toggleLike} />
       <ShareIcon />
       <MenuIcon />
     </MenuComponentWrapper>
